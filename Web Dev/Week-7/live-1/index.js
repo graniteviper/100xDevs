9c8f712d12212ce3389d197987993bc5d0e29354
graniteviper <tanishqs1403@gmail.com>
2024-10-18 17:37:07 +0530

    zod and bcrypt
    Improving the signup and login functionalities and hashing the passwords to upgrade the total security of the backend.

--- Web Dev/Week-7/live-1/index.js
+++ Web Dev/Week-7/live-1/index.js
@@ -1,9 +1,11 @@
-const express = require("express");
+const express = require("express")
 const {UserModel,TodoModel}  = require("./db.js")
 const jwt = require('jsonwebtoken')
-const JWT_SECRET = "s3cret";
+const JWT_SECRET = "s3cret"
 const mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/todos-100xdevs")
+const bcrypt = require('bcrypt')
+const {z} = require('zod')
 
 const app = express();
 app.use(express.json());
@@ -23,22 +25,47 @@ function auth(req,res,next){
 }
 
 app.post("/signup", async function(req, res) {
+    const requiredBody = z.object({
+        email: z.string().min(5).max(100).email(),
+        password: z.string().min(6).max(32),
+        name: z.string().min(5).max(100),
+    })  
+
+    // const parsedData = requiredBody.parse(req.body);
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message:"Incorrect format",
+            error: parsedDataWithSuccess.error
+        })
+        return
+    }
+
+
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
+
+    try {
+        const hashedPassword = await bcrypt.hash(password,5)
+    
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+    } catch (error) {
+        res.json({
+            message:"User already exists."
+        })
+    }
     
     res.json({
         message: "You are signed up"
     })
 });
-
-
 
 
 app.post("/signin", async function(req, res) {
@@ -50,7 +77,15 @@ app.post("/signin", async function(req, res) {
         password: password,
     });
 
-    if (response) {
+    if(!response){
+        res.status(403).json({
+            message:"User does not exist."
+        })
+    }
+
+    const passwordMatch = bcrypt.compare(password,response.password)
+
+    if (passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString()
         },JWT_SECRET)
@@ -90,3 +125,4 @@ app.get("/gettodos",auth, function(req, res) {
 });
 
 app.listen(3000);
+
